8c06b8c4649328c744a8f750b6dffe2a40d4a1dd
Alexandre Junior <alejunior.dev@gmail.com>
2024-08-01 22:00:02 -0300

    feat: create header style

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,19 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+import { Header } from "../components/header";
+import Constants from "expo-constants";
 
 export default function Index() {
+  const statusBarHeight = Constants.statusBarHeight;
+
   return (
-    <View className="flex justify-center items-center">
-      <Text className="bg-green-600">
-        Edit app/index.tsx to edit this screen.
-      </Text>
-    </View>
+    <ScrollView
+      style={{ flex: 1 }}
+      className="bg-slate-200"
+      showsVerticalScrollIndicator={false}
+    >
+      <View style={{ marginTop: statusBarHeight + 8 }} className="w-full px-4">
+        <Header />
+      </View>
+    </ScrollView>
   );
 }
