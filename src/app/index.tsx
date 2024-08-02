c642c96d5d9e74406b3222bfba4b07c989647c9b
Alexandre Junior <alejunior.dev@gmail.com>
2024-08-01 21:19:35 -0300

    feat: config tailwindcss

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex justify-center items-center">
+      <Text className="bg-green-600">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
